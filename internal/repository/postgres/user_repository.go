c480919254e6aed1ceb8c686d601ea173f29d2ae
Sterlingov <forwardeleven13@gmail.com>
2025-09-07 02:39:18 +0300

    feat: add Create and GetByID methods to user repository

--- internal/repository/postgres/user_repository.go
+++ internal/repository/postgres/user_repository.go
@@ -2,7 +2,9 @@ package postgres
 
 import (
 	"database/sql"
+	"fmt"
 
+	"github.com/lib/pq"
 	"github.com/sterlingov/chadder-net-backend/internal/entity"
 	"github.com/sterlingov/chadder-net-backend/internal/repository"
 )
@@ -16,13 +18,56 @@ func NewUserRepo(db *sql.DB) repository.UserRepository {
 }
 
 func (r *userRepo) Create(user *entity.User) (int64, error) {
-	//Затычка
-	return 0, nil
+	query := "INSERT INTO users (name, username, email, bio, password_hash, created_at) VALUES ($1, $2, $3, $4, $5, $6) RETURNING id"
+	var id int64
+
+	err := r.db.QueryRow(query,
+		sql.NullString{String: user.Name, Valid: user.Name != ""},
+		user.Username,
+		user.Email,
+		sql.NullString{String: user.Bio, Valid: user.Bio != ""},
+		user.PasswordHash,
+		user.CreatedAt).Scan(&id)
+
+	if err != nil {
+		if pgErr, ok := err.(*pq.Error); ok && pgErr.Code == "23505" {
+			switch pgErr.Constraint {
+			case "users_email_key":
+				return 0, fmt.Errorf("email already exists")
+			case "users_username_key":
+				return 0, fmt.Errorf("username already exists")
+			default:
+				return 0, fmt.Errorf("duplicate value")
+			}
+		}
+		return 0, err
+	}
+
+	return id, nil
 }
 
 func (r *userRepo) GetByID(id int64) (*entity.User, error) {
-	//Затычка
-	return &entity.User{}, nil
+	query := "SELECT (id, username, name, bio, created_at, avatar) FROM users WHERE id = $1"
+
+	user := &entity.User{}
+	var bio sql.NullString
+	var avatar sql.NullString
+
+	err := r.db.QueryRow(query, id).Scan(
+		&user.ID,
+		&user.Username,
+		&user.Name,
+		&bio,
+		&user.CreatedAt,
+		&avatar)
+
+	if err != nil {
+		return nil, err
+	}
+	user.Avatar = avatar.String
+	user.Bio = bio.String
+
+	return user, nil
 }
 
 func (r *userRepo) GetByUsername(username string) (*entity.User, error) {
